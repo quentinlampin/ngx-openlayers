99558f77d56a7b97c416865f3307ce0058628cb7
Quentin Lampin <Quentin Lampin>
2016-12-12 11:11:58 +0100

    bugfix: map events callbacks are now executed against the MapComponent context

--- src/components/map.component.ts
+++ src/components/map.component.ts
@@ -12,24 +12,36 @@ export class MapComponent{
   public instance: Map;
   public options: any;
 
-  @Output() onClick = new EventEmitter<MapBrowserEvent>();
-  @Output() onDblClick = new EventEmitter<MapBrowserEvent>();
-  @Output() onMoveEnd = new EventEmitter<MapEvent>();
-  @Output() onPointerDrag = new EventEmitter<MapBrowserEvent>();
-  @Output() onPointerMove = new EventEmitter<MapBrowserEvent>();
-  @Output() onPostCompose = new EventEmitter<render.Event>();
-  @Output() onPostRender = new EventEmitter<MapEvent>();
-  @Output() onPreCompose = new EventEmitter<render.Event>();
-  @Output() onPropertyChange = new EventEmitter<ObjectEvent>();
-  @Output() onSingleClick = new EventEmitter<MapBrowserEvent>();
+  @Output() onClick: EventEmitter<MapBrowserEvent>;
+  @Output() onDblClick: EventEmitter<MapBrowserEvent>;
+  @Output() onMoveEnd: EventEmitter<MapEvent>;
+  @Output() onPointerDrag: EventEmitter<MapBrowserEvent>;
+  @Output() onPointerMove: EventEmitter<MapBrowserEvent>;
+  @Output() onPostCompose: EventEmitter<render.Event>;
+  @Output() onPostRender: EventEmitter<MapEvent>;
+  @Output() onPreCompose: EventEmitter<render.Event>;
+  @Output() onPropertyChange: EventEmitter<ObjectEvent>;
+  @Output() onSingleClick: EventEmitter<MapBrowserEvent>;
 
   constructor(element: ElementRef){
+    this._host_ = element;
+
     this.options = {
       target: null,
       controls: []
     };
     this.instance = new Map(this.options);
-    this._host_ = element;
+
+    this.onClick = new EventEmitter<MapBrowserEvent>();
+    this.onDblClick = new EventEmitter<MapBrowserEvent>();
+    this.onMoveEnd = new EventEmitter<MapEvent>();
+    this.onPointerDrag = new EventEmitter<MapBrowserEvent>();
+    this.onPointerMove = new EventEmitter<MapBrowserEvent>();
+    this.onPostCompose = new EventEmitter<render.Event>();
+    this.onPostRender = new EventEmitter<MapEvent>();
+    this.onPreCompose = new EventEmitter<render.Event>();
+    this.onPropertyChange = new EventEmitter<ObjectEvent>();
+    this.onSingleClick = new EventEmitter<MapBrowserEvent>();
   }
 
   ngOnInit(){
@@ -39,35 +51,38 @@ export class MapComponent{
   }
 
   private addEvents() {
+    console.log('instance: ', this.instance);
+    console.log('mapComponent: ', this);
+
     this.instance.on('click', function(event: MapBrowserEvent) {
       this.onClick.emit(event);
-    });
+    }.bind(this));
     this.instance.on('dblclick', function(event: MapBrowserEvent) {
       this.onDblClick.emit(event);
-    });
+    }.bind(this));
     this.instance.on('moveend', function(event: MapEvent) {
       this.onMoveEnd.emit(event);
-    });
+    }.bind(this));
     this.instance.on('pointerdrag', function(event: MapBrowserEvent) {
       this.onPointerDrag.emit(event);
-    });
+    }.bind(this));
     this.instance.on('pointermove', function(event: MapBrowserEvent) {
       this.onPointerMove.emit(event);
-    });
+    }.bind(this));
     this.instance.on('postcompose', function(event: render.Event) {
       this.onPostCompose.emit(event);
-    });
+    }.bind(this));
     this.instance.on('postrender', function(event: MapEvent) {
       this.onPostRender.emit(event);
-    });
+    }.bind(this));
     this.instance.on('precompose', function(event: render.Event) {
       this.onPreCompose.emit(event);
-    });
+    }.bind(this));
     this.instance.on('propertychange', function(event: ObjectEvent) {
       this.onPropertyChange.emit(event);
-    });
+    }.bind(this));
     this.instance.on('singleclick', function(event: MapBrowserEvent) {
       this.onSingleClick.emit(event);
-    });
+    }.bind(this));
   }
 }
