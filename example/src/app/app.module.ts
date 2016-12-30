da9ed8928c76d0f0e1975d721317de6870951af4
Quentin Lampin <Quentin Lampin>
2016-12-30 17:17:23 +0100

    few fixes

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AngularOpenlayersModule } from './ng2-ol';
+import { AngularOpenlayersModule } from 'angular2-openlayers';
 
 import { AppComponent } from './app.component';
 
