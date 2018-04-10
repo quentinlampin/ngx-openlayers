f229c807e44acb06fe85729f1d33ae3773a74fea
Guillaume Gasperi <guillaume.gasperi.external@airbus.com>
2018-04-10 14:36:25 +0200

    feat(tileJSON): add tileJSON component

--- src/components/sources/index.ts
+++ src/components/sources/index.ts
@@ -6,6 +6,7 @@ export * from './vectortile.component';
 export * from './xyz.component';
 export * from './tilewms.component';
 export * from './geojson.component';
+export * from './tilejson.component';
 export * from './tilewmts.component';
 export * from './imagewms.component';
 export * from './imagestatic.component';
---
+++ src/components/sources/tilejson.component.ts
@@ -0,0 +1,26 @@
+import { Component, Host, Input, OnInit, forwardRef } from '@angular/core';
+import { AttributionLike, source, TileLoadFunctionType, tilegrid } from 'openlayers';
+import { LayerTileComponent } from '../layers';
+import { SourceComponent } from './source.component';
+
+@Component({
+  selector: 'aol-source-tilejson',
+  template: `<ng-content></ng-content>`,
+  providers: [
+    { provide: SourceComponent, useExisting: forwardRef(() => SourceTileJSONComponent) }
+  ]
+})
+export class SourceTileJSONComponent extends SourceComponent implements OnInit {
+  instance: source.TileJSON;
+
+  @Input() url: string;
+
+  constructor(@Host() layer: LayerTileComponent) {
+    super(layer);
+  }
+
+  ngOnInit() {
+    this.instance = new source.TileJSON(this);
+    this.host.instance.setSource(this.instance);
+  }
+}
