fc1a1ce7a9a67e572c91dd1977030cca1e3b3c06
davinkevin <davin.kevin@gmail.com>
2018-05-28 11:21:03 +0200

    chore(angular): move to angular 6 version

---
+++ projects/ngx-openlayers/src/lib/sources/osm.component.ts
@@ -0,0 +1,40 @@
+import {Component, Host, forwardRef, Input, AfterContentInit, Optional, OnInit} from '@angular/core';
+import { source, AttributionLike, TileLoadFunctionType } from 'openlayers';
+import { LayerTileComponent } from '../layers/layertile.component';
+import { SourceComponent } from './source.component';
+import { SourceXYZComponent } from './xyz.component';
+import {SourceRasterComponent} from './raster.component';
+
+@Component({
+  selector: 'aol-source-osm',
+  template: `<div class="aol-source-osm"></div>`,
+  providers: [
+    { provide: SourceComponent, useExisting: forwardRef(() => SourceOsmComponent) }
+  ]
+})
+export class SourceOsmComponent extends SourceXYZComponent implements AfterContentInit {
+  instance: source.OSM;
+
+  @Input() attributions: AttributionLike;
+  @Input() cacheSize: number;
+  @Input() crossOrigin: string;
+  @Input() maxZoom: number;
+  @Input() opaque: boolean;
+  @Input() reprojectionErrorThreshold: number;
+  @Input() tileLoadFunction: TileLoadFunctionType;
+  @Input() url: string;
+  @Input() wrapX: boolean;
+
+  constructor(@Host() @Optional() layer: LayerTileComponent,
+              @Host() @Optional() raster?: SourceRasterComponent) {
+    super(layer, raster);
+  }
+
+  ngAfterContentInit() {
+    if (this.tileGridXYZ) {
+      this.tileGrid = this.tileGridXYZ.instance;
+    }
+    this.instance = new source.OSM(this);
+    this._register(this.instance);
+  }
+}
---
+++ projects/ngx-openlayers/src/lib/sources/vectortile.component.ts
@@ -0,0 +1,43 @@
+import { Component, Host, Input, forwardRef, ContentChild, AfterContentInit } from '@angular/core';
+import { source, ProjectionLike, TileUrlFunctionType, format, tilegrid } from 'openlayers';
+import { LayerVectorTileComponent } from '../layers/layervectortile.component';
+import { FormatComponent } from '../formats/format.component';
+import { TileGridComponent } from '../tilegrid.component';
+import { SourceComponent } from './source.component';
+
+@Component({
+  selector: 'aol-source-vectortile',
+  template: `<ng-content></ng-content>`,
+  providers: [
+    { provide: SourceComponent, useExisting: forwardRef(() => SourceVectorTileComponent) }
+  ]
+})
+export class SourceVectorTileComponent extends SourceComponent implements AfterContentInit {
+  public instance: source.VectorTile;
+  @Input() cacheSize: number;
+  @Input() overlaps: boolean;
+  @Input() projection: ProjectionLike;
+  @Input() tilePixelRatio: number;
+  @Input() tileUrlFunction: TileUrlFunctionType;
+  @Input() url: string;
+  @Input() urls: string[];
+  @Input() wrapX: boolean;
+
+  @ContentChild(FormatComponent) formatComponent: FormatComponent;
+  format: format.Feature;
+  @ContentChild(TileGridComponent) tileGridComponent: TileGridComponent;
+  tileGrid: tilegrid.TileGrid;
+
+  constructor(@Host() layer: LayerVectorTileComponent) {
+    super(layer);
+  }
+
+  /* need the children to construct the OL3 object */
+  ngAfterContentInit() {
+    this.format = this.formatComponent.instance;
+    this.tileGrid = this.tileGridComponent.instance;
+    // console.log('creating ol.source.VectorTile instance with:', this);
+    this.instance = new source.VectorTile(this);
+    this.host.instance.setSource(this.instance);
+  }
+}
