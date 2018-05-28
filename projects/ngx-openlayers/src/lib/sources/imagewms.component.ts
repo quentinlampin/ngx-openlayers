ea18db3c3f2723052f79d258c55c9db99443b0c8
davinkevin <davin.kevin@gmail.com>
2018-05-28 11:21:03 +0200

    chore(angular): move to angular 6 version

---
+++ projects/ngx-openlayers/src/lib/sources/imagewms.component.ts
@@ -0,0 +1,36 @@
+import { Component, Host, Input, OnInit, forwardRef } from '@angular/core';
+import { AttributionLike, ImageLoadFunctionType, ProjectionLike, source } from 'openlayers';
+import { LayerImageComponent } from '../layers/layerimage.component';
+import { SourceComponent } from './source.component';
+
+@Component({
+  selector: 'aol-source-imagewms',
+  template: `<ng-content></ng-content>`,
+  providers: [
+    { provide: SourceComponent, useExisting: forwardRef(() => SourceImageWMSComponent) }
+  ]
+})
+export class SourceImageWMSComponent extends SourceComponent implements OnInit {
+  instance: source.ImageWMS;
+
+  @Input() attributions: AttributionLike;
+  @Input() crossOrigin: string;
+  @Input() hidpi: boolean;
+  @Input() serverType: string;
+  @Input() imageLoadFunction?: ImageLoadFunctionType;
+  @Input() logo: (string | olx.LogoOptions);
+  @Input() params: Object;
+  @Input() projection: (ProjectionLike | string);
+  @Input() ratio: number;
+  @Input() resolutions: Array<number>;
+  @Input() url: string;
+
+  constructor(@Host() layer: LayerImageComponent) {
+    super(layer);
+  }
+
+  ngOnInit() {
+    this.instance = new source.ImageWMS(this);
+    this.host.instance.setSource(this.instance);
+  }
+}
---
+++ projects/ngx-openlayers/src/lib/styles/style.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input, Optional, OnInit } from '@angular/core';
+import { style, StyleGeometryFunction, geom } from 'openlayers';
+import { FeatureComponent } from '../feature.component';
+import { LayerVectorComponent } from '../layers/layervector.component';
+
+@Component({
+  selector: 'aol-style',
+  template: `<ng-content></ng-content>`
+})
+export class StyleComponent implements OnInit {
+  private host: FeatureComponent|LayerVectorComponent;
+  public instance: style.Style;
+  public componentType: string = 'style';
+
+  @Input() geometry: string | geom.Geometry | StyleGeometryFunction;
+  @Input() fill: style.Fill;
+  @Input() image: style.Image;
+  @Input() stroke: style.Stroke;
+  @Input() text: style.Text;
+  @Input() zIndex: number;
+
+  constructor(
+    @Optional() featureHost: FeatureComponent,
+    @Optional() layerHost: LayerVectorComponent
+  ) {
+    // console.log('creating aol-style');
+    this.host = !!featureHost ? featureHost : layerHost;
+    if (!this.host) {
+      throw new Error('aol-style must be applied to a feature or a layer');
+    }
+  }
+
+  update() {
+    // console.log('updating style\'s host: ', this.host);
+    this.host.instance.changed();
+  }
+
+  ngOnInit() {
+    // console.log('creating aol-style instance with: ', this);
+    this.instance = new style.Style(this);
+    this.host.instance.setStyle(this.instance);
+  }
+}
