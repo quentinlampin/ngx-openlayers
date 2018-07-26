2bf558c29c66bd5abe7d7df07482c279ba209735
Clara Belair <clara.belair.external@airbus.com>
2018-07-26 11:38:14 +0200

    feat(demo): example draw a polygon

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,6 +9,7 @@ import { MapPositionComponent } from './map-position/map-position.component';
 import { CursorPositionComponent } from './cursor-position/cursor-position.component';
 import { DisplayGeometryComponent } from './display-geometry/display-geometry.component';
 import { DisplayGeojsonSourceComponent } from './display-geojson-source/display-geojson-source.component';
+import { DrawPolygonComponent } from './draw-polygon/draw-polygon.component';
 
 const routes: Routes = [
   { path: '', component: ExamplesListComponent },
@@ -21,6 +22,7 @@ const routes: Routes = [
       { path: 'cursor-position', component: CursorPositionComponent },
       { path: 'display-geometry', component: DisplayGeometryComponent },
       { path: 'display-geojson-source', component: DisplayGeojsonSourceComponent },
+      { path: 'draw-polygon', component: DrawPolygonComponent },
       { path: 'cluster', component: ClusterComponent },
       { path: 'raster', component: RasterComponent },
     ],
