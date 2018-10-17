4dc9852503f7940bd6e00685bb6c70f07284cb17
Samuel Girard <samuel.girard@gmail.com>
2018-10-17 12:11:44 +0200

    feat(overviewmap): refresh overview when the view changes

---
+++ src/app/overview/overview.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <aol-map width="50%" height="50%">
+      <aol-interaction-default></aol-interaction-default>
+      <aol-control-defaults></aol-control-defaults>
+      <aol-control-overviewmap></aol-control-overviewmap>
+      <aol-view #view [zoom]="zoom">
+        <aol-coordinate [x]="5" [y]="45" [srid]="'EPSG:4326'"></aol-coordinate>
+      </aol-view>
+      <aol-layer-tile #osm [opacity]="1"> <aol-source-osm></aol-source-osm> </aol-layer-tile>
+    </aol-map>
+  `,
+})
+export class OverviewComponent {
+  public zoom = 15;
+}
