410f8767ed44ab028ed594fe7910851c91503c17
Clara Belair <clara.belair@orange.fr>
2019-06-03 10:48:52 +0200

    feat(source:imagestatic): refresh layer source when url change

--- src/app/image-static/image-static.component.ts
+++ src/app/image-static/image-static.component.ts
@@ -8,24 +8,40 @@ import Projection from 'ol/proj/Projection';
     <aol-map [width]="'100%'" [height]="'100%'">
       <aol-view [projection]="projection" [zoom]="2" [center]="[512, 484]"> </aol-view>
       <aol-layer-image [opacity]="opacity" [extent]="extent">
-        <aol-source-imagestatic
-          [url]="'https://imgs.xkcd.com/comics/online_communities.png'"
-          [projection]="projection"
-          [imageExtent]="extent"
-        >
-        </aol-source-imagestatic>
+        <aol-source-imagestatic [url]="url" [projection]="projection" [imageExtent]="extent"> </aol-source-imagestatic>
       </aol-layer-image>
     </aol-map>
+    <div class="controls">
+      Current image url:
+      <select (change)="onUrlChange($event)">
+        <option value="https://imgs.xkcd.com/comics/online_communities.png"
+          >https://imgs.xkcd.com/comics/online_communities.png</option
+        >
+        <option value="https://pbs.twimg.com/media/D7IgamEUEAA5DHE.jpg"
+          >https://pbs.twimg.com/media/D7IgamEUEAA5DHE.jpg</option
+        >
+      </select>
+    </div>
   `,
   styles: [
     `
-      map {
-        background: #e0eced;
+      :host {
+        display: flex;
+      }
+
+      aol-map {
+        width: 70%;
+      }
+
+      .controls {
+        width: 28%;
+        padding: 1rem;
       }
     `,
   ],
 })
 export class ImageStaticComponent {
+  public url = 'https://imgs.xkcd.com/comics/online_communities.png';
   public zoom = 5;
   public opacity = 1.0;
   extent: Extent = [0, 0, 1024, 968];
@@ -39,4 +55,8 @@ export class ImageStaticComponent {
   projection = new Projection(this.po);
 
   getCenter = ext => Extent.getCenter(ext);
+
+  onUrlChange(evt) {
+    this.url = evt.target.value;
+  }
 }
