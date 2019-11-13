0fe928fd757923f4e8df2855cd02500063d6ec9a
Clara Belair <clara.belair.external@airbus.com>
2019-11-13 15:46:40 +0100

    feat(wmts): add wmts tile load events

--- src/app/select-interaction/select-interaction.component.ts
+++ src/app/select-interaction/select-interaction.component.ts
@@ -6,33 +6,33 @@ import { SelectEvent } from 'ol/interaction/Select';
 @Component({
   selector: 'app-select-interaction',
   template: `
-      <aol-map #map width="100%" height="100%">
-          <aol-interaction-default></aol-interaction-default>
+    <aol-map #map width="100%" height="100%">
+      <aol-interaction-default></aol-interaction-default>
 
-          <aol-interaction-select
-                  [layers]="isMarkerLayer"
-                  (olSelect)="select($event)"
-                  [wrapX]="false"
-          ></aol-interaction-select>
+      <aol-interaction-select
+        [layers]="isMarkerLayer"
+        (olSelect)="select($event)"
+        [wrapX]="false"
+      ></aol-interaction-select>
 
-          <aol-view [zoom]="5">
-              <aol-coordinate [x]="1.4886" [y]="43.5554" [srid]="'EPSG:4326'"></aol-coordinate>
-          </aol-view>
+      <aol-view [zoom]="5">
+        <aol-coordinate [x]="1.4886" [y]="43.5554" [srid]="'EPSG:4326'"></aol-coordinate>
+      </aol-view>
 
-          <aol-layer-tile [opacity]="1">
-              <aol-source-osm></aol-source-osm>
-          </aol-layer-tile>
+      <aol-layer-tile [opacity]="1">
+        <aol-source-osm></aol-source-osm>
+      </aol-layer-tile>
 
-          <aol-layer-vector #markersLayer>
-              <aol-source-vector #markersSource [wrapX]="false">
-                  <aol-feature>
-                      <aol-geometry-point>
-                          <aol-coordinate [x]="5" [y]="45" [srid]="'EPSG:4326'"></aol-coordinate>
-                      </aol-geometry-point>
-                  </aol-feature>
-              </aol-source-vector>
-          </aol-layer-vector>
-      </aol-map>
+      <aol-layer-vector #markersLayer>
+        <aol-source-vector #markersSource [wrapX]="false">
+          <aol-feature>
+            <aol-geometry-point>
+              <aol-coordinate [x]="5" [y]="45" [srid]="'EPSG:4326'"></aol-coordinate>
+            </aol-geometry-point>
+          </aol-feature>
+        </aol-source-vector>
+      </aol-layer-vector>
+    </aol-map>
   `,
 })
 export class SelectInteractionComponent {
