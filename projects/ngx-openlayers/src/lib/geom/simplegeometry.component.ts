5ae73134c4e2f1c276da35f6ae65273fdde1548c
Jeremy Eychenne <jeremy.eychenne@airbus.com>
2023-03-15 15:59:39 +0100

    chore: migrate to angular 10

--- projects/ngx-openlayers/src/lib/geom/simplegeometry.component.ts
+++ projects/ngx-openlayers/src/lib/geom/simplegeometry.component.ts
@@ -1,8 +1,10 @@
-import { Input, OnInit } from '@angular/core';
+import { Directive, Input, OnInit } from '@angular/core';
 import { FeatureComponent } from '../feature.component';
 import { MapComponent } from '../map.component';
 import SimpleGeometry from 'ol/geom/SimpleGeometry';
 
+@Directive()
+// tslint:disable-next-line:directive-class-suffix
 export abstract class SimpleGeometryComponent implements OnInit {
   public instance: SimpleGeometry;
   public componentType = 'simple-geometry';
