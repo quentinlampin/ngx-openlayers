c2ae59a3c85c40e6867ce90ec9819d9447d9730a
Jeremy Eychenne <jeremy.eychenne@airbus.com>
2023-04-05 11:14:21 +0200

    chore: migrate to angular 11

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,11 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
+  async navigateTo(): Promise<unknown> {
     return browser.get('/');
   }
 
-  getParagraphText() {
+  async getParagraphText(): Promise<string> {
     return element(by.css('app-root h1')).getText();
   }
 }
