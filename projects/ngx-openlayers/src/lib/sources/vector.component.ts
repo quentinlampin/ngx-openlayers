0bda38a7ef0233f9e7f01ca8c39dfe53504d15e0
Mlhotellerie <matthieu.lhotellerie@airbus.com>
2024-04-24 15:33:48 +0200

    feat(source-vector): support of 'loader' input

--- projects/ngx-openlayers/src/lib/sources/vector.component.ts
+++ projects/ngx-openlayers/src/lib/sources/vector.component.ts
@@ -1,9 +1,13 @@
 import { Component, forwardRef, Host, Input, OnInit } from '@angular/core';
 import { Vector } from 'ol/source';
-import Feature from 'ol/format/Feature';
+import { default as FeatureFormat } from 'ol/format/Feature';
 import { LayerVectorComponent } from '../layers/layervector.component';
 import { SourceComponent } from './source.component';
 import { LoadingStrategy } from 'ol/source/Vector';
+import { Geometry } from 'ol/geom';
+import { Extent } from 'ol/extent';
+import { Projection } from 'ol/proj';
+import { Feature } from 'ol';
 
 @Component({
   selector: 'aol-source-vector',
@@ -20,9 +24,28 @@ export class SourceVectorComponent extends SourceComponent implements OnInit {
   @Input()
   url: string;
   @Input()
-  format: Feature;
+  format: FeatureFormat;
   @Input()
   strategy: LoadingStrategy;
+
+  /**
+   * The loader function used to load features, from a remote source for example.
+   * If this is not set and `url` is set, the source will create and use an XHR
+   * feature loader. The `'featuresloadend'` and `'featuresloaderror'` events
+   * will only fire if the `success` and `failure` callbacks are used.
+   *
+   * An extra argument is provided to the OpenLayers callback to enable retrieval
+   * of the parent VectorSource.
+   */
+  @Input()
+  loader: (
+    extent: Extent,
+    resolution: number,
+    projection: Projection,
+    success: (geos: Feature<Geometry>[]) => void,
+    failure: () => void,
+    vectorSource: Vector
+  ) => void;
 
   instance: Vector;
 
@@ -31,7 +54,14 @@ export class SourceVectorComponent extends SourceComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.instance = new Vector(this);
+    this.instance = new Vector({
+      ...this,
+      loader: this.loader
+        ? (extent, resolution, projection, success, failure) =>
+            this.loader(extent, resolution, projection, success, failure, this.instance)
+        : undefined,
+    });
+
     this.host.instance.setSource(this.instance);
   }
 }
